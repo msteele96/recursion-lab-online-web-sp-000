a83b4d4502636e054970e0e165a657e646e686b9
Marc Steele <marcsteele2014@gmail.com>
2021-06-07 19:03:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function printString(myString) {
   console.log(myString[0]);
- 
+
   if (myString.length > 1) {
     let mySubString = myString.substring(1, myString.length);
     printString(mySubString);
