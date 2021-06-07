3b7cd8aae37d8580e2d42dcc081057a42a82dfbb
Marc Steele <marcsteele2014@gmail.com>
2021-06-07 19:27:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
-// Code your solution here!
+function printString(string) {
+  console.log(string[0]);
 
+  if (string.length > 1) {
+    let subString = string.substring(1, string.length);
+    printString(subString);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(string) {
+  if (string.length < 2) {
+    return string;
+  } else {
+    return reverseString(string.substring(1)) + string[0];
+  }
+}
+
+function isPalindrome(string) {
+  if (string.length < 2) {
+    return true;
+  } else if (string[0] === string[string.length -1]) {
+    return isPalindrome(string.substring(1, string.length-1))
+  } else {
+    return false
+  }
+}
+
+function addUpTo(myArray, index) {
+  return index ? myArray[index] + addUpTo(myArray, --index) : myArray[index];
+}
+
+function maxOf(myArray) {
+  if (myArray.length === 1) {
+    return myArray[0];
+  } else {
+    return Math.max(myArray.pop(), maxOf(myArray));
+  }
+}
+
+function includesNumber(myArray, myNumber) {
+  if (!myArray.length) {
+    return false;
+  } else if (myArray[0] === myNumber) {
+    return true;
+  } else {
+    return includesNumber(myArray.slice(1), myNumber);
+  }
+}
