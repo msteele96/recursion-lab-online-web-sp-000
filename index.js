6c94c7110b4a7f13d510068e05a6f68331e48788
Marc Steele <marcsteele2014@gmail.com>
2021-06-07 18:57:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 // Code your solution here!
+const printString = () => {
+  
+}
 
+const reverseString = () => {
+  
+}
+
+const isPalindrome = () => {
+  
+}
+
+const addUpTo = () => {
+  
+}
+
+const maxOf = () => {
+  
+}
+
+const includesNumber = () => {
+  
+}
